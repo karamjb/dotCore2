dd973b374ee0d3af78ac6bffb4fd523f98690b7c
Karam Jabareen Home <karam@arab-creative.com>
2018-04-04 00:27:51 +0300

    ym ii

---
+++ WebApplication2/Models/Post.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        [Required]
+        [StringLength(200)]
+        public string Subject { get; set; }
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Body { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime PublicationDate { get; set; }
+        public string AuthorName { get; set; }
+        public int Readers { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
